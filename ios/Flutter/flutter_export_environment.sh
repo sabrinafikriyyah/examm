1e7bc280bb320c1051d66ded40d949ce96480887
sabrinafikriyyah <160982389+sabrinafikriyyah@users.noreply.github.com>
2024-03-06 14:34:03 +0700

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\LAB B2\Downloads\flutter_windows_3.13.9-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\XI PPLG 2\bu yayak\flutter_application_1exam"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
